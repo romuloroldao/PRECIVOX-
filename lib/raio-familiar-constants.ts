8199419332263b93bfc0f0a401a5fb8b083c0926
romuloroldao <romuloroldao@gmail.com>
2026-07-28 16:19:59 -0300

    fix(cliente): exportar constantes da sugestão de compartilhar Casa
    CasaCompartilharBanner já usava CASA_SUGESTAO_* sem estarem no módulo versionado.

--- lib/raio-familiar-constants.ts
+++ lib/raio-familiar-constants.ts
@@ -4,3 +4,7 @@
 
 export const RAIO_FAMILIAR_MAX_MEMBROS = 6;
 export const RAIO_FAMILIAR_CODIGO_LEN = 6;
+/** Itens na lista antes de sugerir compartilhar com a casa */
+export const CASA_SUGESTAO_MIN_ITENS_LISTA = 3;
+export const CASA_SUGESTAO_SNOOZE_KEY = 'precivox_casa_sugestao_snooze_until';
+export const CASA_SUGESTAO_SNOOZE_MS = 7 * 24 * 60 * 60 * 1000;
